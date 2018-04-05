79a186a2706d8fffb52ca86fd69e7f05b0fb961d
Phil Stavri <p.stavri@gmail.com>
2018-04-05 23:44:54 +0100

    Refactor Welcome view to use Flaoting Page component

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,24 +1,20 @@
 <template>
-  <div class="welcome">
-    <div class="welcome__content">
-      <h1>Welcome</h1>
-      <p>Use this tool to search Nasa's images and videos</p>
-    </div>
-  </div>
+  <floating-page title="Welcome to NASA Search">
+      <p>You can use this tool to search and view NASA's images and videos</p>
+      <router-link to="/search">Go to search</router-link>
+  </floating-page>
 </template>
-<style lang="scss">
-.welcome {
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.welcome__content{
-  width: 40%;
-  min-width: 320px;
-  background-color: rgba(200,200,200, 0.7);
-  color: #444;
-  border-radius: 5px;
-  padding: 20px;
-}
-</style>
+<script>
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+// @ is an alias to /src
+import FloatingPage from '@/components/FloatingPage.vue'
+
+@Component({
+  components: {
+    FloatingPage
+  }
+})
+export default class Welcome extends Vue {}
+</script>
